5e7bf98c4cb97062d7ed4c9a40dfaf5064a394af
HackYourFuture <hackyourfuture@MacBook-Pro-van-HackYourFuture.local>
2024-04-11 23:16:23 +0200

    3

---
+++ bigInt.js
@@ -0,0 +1,10 @@
+function int(num1,num2) {
+    if (num1>num2) {
+      return (`${num1} is bigger`)
+    } else {
+        return(`${num2} is bigger`)
+        
+    }
+}
+console.log(int(33,79))
+// Write a JavaScript program that displays the largest integer among two integers.
